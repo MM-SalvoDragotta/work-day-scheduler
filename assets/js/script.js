064bbd0d0aad6ac64635afb6a535b08d667e31c6
SalvoD <salvo.dragotta@mottmac.com>
2021-07-04 23:33:07 +1000

    Generate hours with JQUERY

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,58 +13,49 @@ $(document).ready(function() {
   setInterval(datetime_update, 1000);
 });
 
-const currentHour = parseInt(moment().format("H"));
+// const currentHour = parseInt(moment().format("H"));
+
+const currentHour = 10;
+
+function generateHour (length , start){
+    for (var i=0 ; i<length ; i++){
+        $(".container")
+        .append(`
+        <div class='row h-75 '> 
+            <div id="${i+start}" class="ppf col-sm-2 border border-dark rounded text-centre">${i+start} </div>
+            <input id="${i+start}" data-name="${"localStorage"}${i+start}" class="ppf col-sm-8 border border-dark form-control form-control-lg" type="text" placeholder=".form-control-lg"> </input>
+            <div id="${i+start}" class="ppf col-sm-2 border border-dark rounded text-centre"> <span id="btn"> Save </span> </div>
+        </div>`);
+    }      
+}
+ 
+generateHour (10,8);
+
+
 
 var selectAllHours = $(".ppf");
-
+    
 $.each(selectAllHours, function (i, ppf) {
     var getId = parseInt($(this).attr("id"));
-    console.log(getId)
+    // console.log(getId)
     if (getId === currentHour) {
       $(this).next().addClass("present");
-      console.log(getId)
+    //   console.log(getId)
     } else if (getId < currentHour) {
       $(this).next().addClass("past");
-      console.log(getId)
+    //   console.log(getId)
     } else if (getId > currentHour) {
       $(this).next().addClass("future");
-      console.log(getId)
+    //   console.log(getId)
     }
-  });
+  });    
+       
+     
+     
+       
   
 
-
-// $(".container")
-// .append(
-//     $("<div class='row h-75'>") )
-// $(".row")
-// .append(
-//     $("<div id='9'class='col-sm-2 border border-dark rounded'>")
-// )
-//     .append(
-//         $("<div id='9'class='col-sm-2 border border-dark rounded'> 9AM </div>")
-//         .append(
-//             $("<input id='9'class='col-sm-8 border border-dark form-control form-control-lg' type='text' placeholder='.form-control-lg'></input> ")
-//             .append(
-//                 $("<div id='9' class='col-sm-2 border border-dark rounded'>Save </div>")
-//                 .append(
-//                     $("</div>")
-//                 )
-
-//             )
-//         )
-//     )
-// )
-
-// $(".eight").addClass("present")
-
-/* <div class="row h-75">
-<div class="time8am col-sm-2 border border-dark rounded">
-  8AM
-</div>
-<input class="time8am col-sm-8 border border-dark form-control form-control-lg" type="text" placeholder=".form-control-lg">  
-<!-- <textarea class="col-sm-8 border border-dark future" id="w3review" name="w3review" ></textarea>   -->            
-<div id = "8" class="time8am col-sm-2 border border-dark rounded">
-  Save
-</div>
-</div> */
+     
+       
+     
+   
