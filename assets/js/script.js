c877ddfeee1acbb655e448f46b380c85f1f1e942
SalvoD <salvo.dragotta@mottmac.com>
2021-07-05 00:02:25 +1000

    Change Hour display to AM PM

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ date = null;
 //Show date and Time
 var datetime_update = function() {
   date = moment(new Date());
-  datetime.html(date.format('ddd [, ] Do MMMM YYYY HH:mm:ss'));
+  datetime.html(date.format('ddd [, ] Do MMMM YYYY hh:mm:ss A'));
 };
 
 $(document).ready(function() {
@@ -17,14 +17,16 @@ $(document).ready(function() {
 
 const currentHour = 10;
 
+const hourArray = ["8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
+
 function generateHour (length , start){
     for (var i=0 ; i<length ; i++){
         $(".container")
         .append(`
         <div class='row h-75 '> 
-            <div id="${i+start}" class="ppf col-sm-2 border border-dark rounded text-centre">${i+start} </div>
-            <input id="${i+start}" data-name="${"localStorage"}${i+start}" class="ppf col-sm-8 border border-dark form-control form-control-lg" type="text" placeholder=".form-control-lg"> </input>
-            <div id="${i+start}" class="ppf col-sm-2 border border-dark rounded text-centre"> <span id="btn"> Save </span> </div>
+            <div id="${i+start}" class="ppf col-sm-2 border border-dark rounded text-center">${hourArray[i]} </div>
+            <input id="${i+start}" data-name="${"localStorage"}${i+start}" class="ppf col-sm-8 border border-dark form-control form-control-lg" type="text" placeholder=".form-control-lg">
+            <div id="${i+start}" class="ppf col-sm-2 border border-dark rounded text-center"> <span id="btn"> Save </span> </div>
         </div>`);
     }      
 }
