3beca9422b454a2f5b2c47a6c880f2efc7136759
SalvoD <salvo.dragotta@mottmac.com>
2021-07-05 12:30:47 +1000

    Implement renderLocalStorage, refactor script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 var datetime = null;
 date = null;
 
+const currentHour = parseInt(moment().format("H"));
+// const currentHour = 10;
+
+const hourArray = ["8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM"];
+
 //Show date and Time
 var datetime_update = function() {
   date = moment(new Date());
@@ -13,63 +18,58 @@ $(document).ready(function() {
   setInterval(datetime_update, 1000);
 });
 
-// const currentHour = parseInt(moment().format("H"));
-
-const currentHour = 10;
-
-const hourArray = ["8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
-
+//https://stackoverflow.com/questions/6623231/remove-all-white-spaces-from-text
 function generateHour (length , start){
     for (var i=0 ; i<length ; i++){
         $(".container")
         .append(`
         <div class='row h-75 '> 
             <div id="${i+start}" class="ppf col-sm-2 border border-dark rounded text-center">${hourArray[i]} </div>
-            <input id="${i+start}" data-hour="${hourArray[i].replace(/\s/g, '')}" class="ppf col-sm-8 border border-dark form-control form-control-lg" type="text" placeholder=".form-control-lg">
+            <input id="${i+start}" data-hour="${hourArray[i].replace(/\s/g, '')}" class="ppf col-sm-8 border border-dark form-control form-control-lg" type="text" placeholder="Add Task">
             <div id="${i+start}" class="ppf col-sm-2 border border-dark rounded text-center"> <span class="btn"> Save </span> </div>
         </div>`);
     }      
 }
  
-// function generateHour (length , start){
-//     for (var i=0 ; i<length ; i++){
-//         $(".container")
-//         .append(`
-//         <div class='row h-75 '> 
-//             <div id="${i+start}" class="ppf col-sm-2 border border-dark rounded text-center">${hourArray[i]} </div>
-//             <input id="${i+start}" data-name="${"localStorage"}${i+start}" class="ppf col-sm-8 border border-dark form-control form-control-lg" type="text" placeholder=".form-control-lg">
-//             <div id="${i+start}" class="ppf col-sm-2 border border-dark rounded text-center"> <span class="btn"> Save </span> </div>
-//         </div>`);
-//     }      
-// }
+function renderPastPresentFuture(){
+    var selectAllHours = $(".ppf");
 
-
-generateHour (10,8);
-
-
-
-var selectAllHours = $(".ppf");
-    
-$.each(selectAllHours, function (i, ppf) {
-    var getId = parseInt($(this).attr("id"));
-    // console.log(getId)
-    if (getId === currentHour) {
-      $(this).addClass("present");
-    //   console.log(getId)
-    } else if (getId < currentHour) {
-      $(this).addClass("past");
-    //   console.log(getId)
-    } else if (getId > currentHour) {
-      $(this).addClass("future");
-    //   console.log(getId)
-    }
-  });
-  
-function storeScores(input) {
-// Stringify and set key in localStorage to theScores array
-localStorage.setItem("theScores", JSON.stringify(theScores));
+    //This will puch the past present future class to each row.
+    $.each(selectAllHours, function (i, ppf) {
+        var getId = parseInt($(this).attr("id"));
+        // console.log(getId)
+        if (getId === currentHour) {
+        $(this).addClass("present");
+        //   console.log(getId)
+        } else if (getId < currentHour) {
+        $(this).addClass("past");
+        //   console.log(getId)
+        } else if (getId > currentHour) {
+        $(this).addClass("future");
+        //   console.log(getId)
+        }
+    });
 }
-       
+
+function renderLocalStorage(){
+    var inputUserHours = document.getElementsByTagName('input');    
+
+    for (var i=0; i< hourArray.length ; i++) {
+        var localStorageValue = JSON.parse(localStorage.getItem(hourArray[i].replace(/\s/g, '')));
+        inputUserHours[i].value = localStorageValue 
+    }
+}
+
+function init() {    
+    generateHour (10,8);
+    renderPastPresentFuture()
+    renderLocalStorage();     
+}
+
+// save event to save to localStorage 
+  
+init(); 
+
 $(".btn").click(function(event){
     // console.log(event.target.parentElement.previousElementSibling);
     // console.log(event.target.parentElement.previousElementSibling.id)
@@ -80,10 +80,9 @@ $(".btn").click(function(event){
     var storageName = event.target.parentElement.previousElementSibling.getAttribute("data-hour");
     var storageValue = event.target.parentElement.previousElementSibling.value
     localStorage.setItem(storageName, JSON.stringify(storageValue));
-});   
-     
+});  
 
-  
+
 
      
        
